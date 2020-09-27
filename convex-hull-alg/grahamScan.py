ace21b06c26839350f1129968d690d0555bd739d
Parsia <phedayat@Parsias-iMac.local>
2020-09-26 22:04:50 -0500

    Now runs the Graham Scan in both forward and reversed direction of the points, starting with p. Now completes a whole loop, usually creating the hull, except with some intersections. Added functions for a convex hull algorithm written in Python from a Gist.

--- convex-hull-alg/grahamScan.py
+++ convex-hull-alg/grahamScan.py
@@ -2,6 +2,7 @@ import point
 import math
 import random
 import turtle
+from functools import reduce
 
 N = 100
 L = 1
@@ -44,30 +45,61 @@ def drawHull(hl):
 for i in range(N):
     points.append(point.Point(random.randint(L, U), random.randint(L, U)))
 
-newPoints = sorted(points, key=lambda x: x.getY())
+def grahamScanParsia():
+    global points
+    newPoints = sorted(points, key=lambda x: x.getY())
 
-min = 0
-for i in range(len(newPoints)):
-    if newPoints[i].getX() < newPoints[min].getX() and newPoints[i].getY() == newPoints[min].getY():
-        min = i
+    min = 0
+    for i in range(len(newPoints)):
+        if newPoints[i].getX() < newPoints[min].getX() and newPoints[i].getY() == newPoints[min].getY():
+            min = i
 
-p = newPoints[min]
+    p = newPoints[min]
 
-hull = [p]
+    hull = [p]
 
-points = sorted(points, key=lambda x: point.Point.angleBetween(x, p))
-
-for i in points:
-    while len(hull) > 1 and ccw(hull[1], hull[0], i) == 0:
-        hull.pop(0)
+    points = sorted(points, key=lambda x: point.Point.angleBetween(x, p))
     
-    hull.insert(0, i)
 
-for i in hull:
-    print(i.getValue())
+    for i in points:
+        while len(hull) > 1 and ccw(hull[1], hull[0], i) == 0:
+            hull.pop(0)
+        
+        hull.insert(0, i)
 
-drawPoints(points)
-drawHull(hull)
-# angularList = sorted(points, key=lambda x: x)
+    # for i in hull:
+    #     print(i.getValue())
+
+    points = sorted(points, key=lambda x: point.Point.angleBetween(x, p), )
+    for i in reversed(points):
+        while len(hull) > 1 and ccw(hull[1], hull[0], i) == 0:
+            hull.pop(0)
+        
+        hull.insert(0, i)
+
+    drawPoints(points)
+    drawHull(hull)
+
+#####################################################
+
+def turn(p1, p2, p3):
+    z = (p2.getX() - p1.getX())*(p3.getY() - p1.getY()) - (p3.getX() - p1.getX())*(p2.getY() - p1.getY())
+    return cmp(z, 0)
+
+def _keep_left(pl, r):
+    while len(pl) > 1 and turn(pl[-2], pl[-1], r) <= 0:
+        pl.pop()
+    if not len(pl) or pl[-1] != r:
+        pl.append(r)
+    
+    return pl
+
+def grahamScanTix(pl):
+    l = reduce(_keep_left, pl, [])
+    u = reduce(_keep_left, reversed(pl), [])
+    return l.extend(u[i] for i in range(1, len(u) - 1)) or l
+
+if __name__=='__main__':
+    grahamScanParsia()
 
 input('wait')

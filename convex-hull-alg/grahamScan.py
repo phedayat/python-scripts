f6d439953d28d4b872e35db75c026560942196e6
Parsia <phedayat@Parsias-iMac.local>
2020-09-27 02:07:22 -0500

    Cleaned up the code *a lot*. Made as much of it into reusable functions as possible. Added doc. comments to all the functions. Cleaned up extraneous comments and debug prints and unsued import statements.

--- convex-hull-alg/grahamScan.py
+++ convex-hull-alg/grahamScan.py
@@ -1,8 +1,6 @@
 import point
-import math
 import random
 import turtle
-from functools import reduce
 
 N = 100
 L = 1
@@ -12,7 +10,37 @@ points = []
 t = turtle.Turtle()
 t.speed(75)
 
+####### Functions #######
+
+def findMinPoint(pl):
+    '''
+    Find the point with the smallest y-value. Ties are resolved by
+    smallest x-value
+
+    @param pl The list of points
+
+    @return The point with smallest y-value (and x-value)
+    '''
+    newPoints = sorted(points, key=lambda x: x.getY())
+    min = 0
+
+    for i in range(len(newPoints)):
+        if newPoints[i].getY() == newPoints[min].getY() and newPoints[i].getX() < newPoints[min].getX():
+            min = i
+
+    return newPoints[min]
+
 def ccw(p1, p2, p3):
+    '''
+    Determine if the points passed in constitute a counterclockwise turn
+
+    @param p1 The first point. The first point of the first edge
+    @param p1 The second point. The second point of the first edge
+        and first point of the second edge
+    @param p1 The third point. The second point of the second edge
+
+    @return 1 if CCW, otherwise 0
+    '''
     z = (p2.getX() - p1.getX())*(p3.getY() - p1.getY()) - (p2.getY() - p1.getY())*(p3.getX() - p1.getX())
 
     if z > 0:
@@ -20,7 +48,58 @@ def ccw(p1, p2, p3):
     else:
         return 0
 
+def createHull(pl, minP):
+    '''
+    Create the list of points that make up the hull
+
+    @param pl The list of all points
+    @param minP The point with the smallest y-value (and x-value)
+
+    @return A list of points that make up the hull
+    '''
+    hull = [minP]
+
+    pl = sorted(pl, key=lambda x: point.Point.angleBetween(x, minP))
+
+    # First half of hull
+    for i in pl:
+        while len(hull) > 1 and ccw(hull[1], hull[0], i) == 0:
+            hull.pop(0)
+        
+        hull.insert(0, i)
+    
+    # Second half of hull
+    for i in reversed(pl):
+        while len(hull) > 1 and ccw(hull[1], hull[0], i) == 0:
+            hull.pop(0)
+        
+        hull.insert(0, i)
+    
+    return hull
+
+def grahamScan(pl):
+    '''
+    Find the convex hull of a list of points using a Graham Scan
+
+    @param pl The list of points
+    '''
+    # Find point with smallest y-value. Ties are resolved by lowest 
+    # x-value
+    p = findMinPoint(pl)
+
+    # Create the hull
+    hull = createHull(pl, p)
+
+    # Draw the points and the hull
+    drawPoints(pl)
+    drawHull(hull)
+
 def drawPoints(pl):
+    '''
+    Draw the points in the list. First point is always red
+
+    @param pl The list of points to draw
+    '''
     t.penup()
 
     for i in range(len(pl)):
@@ -35,79 +114,24 @@ def drawPoints(pl):
         t.penup()
 
 def drawHull(hl):
+    '''
+    Draw the edges of the hull
+
+    @param hl The list of points in the hull
+    '''
     t.pendown()
 
     for i in range(len(hl)):
         t.goto(hl[i].getX(), hl[i].getY())
     t.penup()
 
+####### End Functions #######
+
 # Create points
 for i in range(N):
     points.append(point.Point(random.randint(L, U), random.randint(L, U)))
 
-newPoints = sorted(points, key=lambda x: x.getY())
-
-min = 0
-for i in range(len(newPoints)):
-    if newPoints[i].getX() < newPoints[min].getX() and newPoints[i].getY() == newPoints[min].getY():
-        min = i
-
-p = newPoints[min]
-
-hull = [p]
-
-points = sorted(points, key=lambda x: point.Point.angleBetween(x, p))
-
-
-for i in points:
-    while len(hull) > 1 and ccw(hull[1], hull[0], i) == 0:
-        hull.pop(0)
-    
-    hull.insert(0, i)
-
-# for i in hull:
-#     print(i.getValue())
-
-points = sorted(points, key=lambda x: point.Point.angleBetween(x, p), )
-for i in reversed(points):
-    while len(hull) > 1 and ccw(hull[1], hull[0], i) == 0:
-        hull.pop(0)
-    
-    hull.insert(0, i)
-
-drawPoints(points)
-drawHull(hull)
-
-#####################################################
-
-# def turn(p1, p2, p3):
-#     z = (p2.getY() - p1.getY())*(p3.getX() - p1.getX()) - (p3.getY() - p1.getY())*(p2.getX() - p1.getX())
-#     if z < 0:
-#         return -1
-#     elif z == 0:
-#         return 0
-#     else:
-#         return 1
-
-# def _keep_left(pl, r):
-#     while len(pl) > 1 and turn(pl[-2], pl[-1], r) != 0:
-#         pl.pop()
-#     if not len(pl) or pl[-1] != r:
-#         pl.append(r)
-    
-#     return pl
-
-# def grahamScanTix(pl):
-#     l = []
-#     u = []
-
-#     for i in range(len(pl)):
-#         l.append(_keep_left(pl, pl[i]))
-
-#     for i in range(len(pl)):
-#         u.append(_keep_left(pl[::-1], pl[-i]))
-
-#     l.extend(u[i] for i in range(1, len(u) - 1))
-#     return l
+if __name__ == '__main__':
+    grahamScan(points)
 
 input('wait')
